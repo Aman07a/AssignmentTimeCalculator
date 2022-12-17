c465b2810c9dd8c48772f3a943d3098d05a01952
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-17 15:58:56 +0100

    Submitted Assignment: TimeCalculator

--- AssignmentTimeCalculator/Program.cs
+++ AssignmentTimeCalculator/Program.cs
@@ -12,7 +12,33 @@ namespace AssignmentTimeCalculator
 	{
 		static void Main(string[] args)
 		{
-			//
+			// Input
+			int seconds = 288970;
+
+			// Copying the actual input; so that, the original input will be persisted
+			int remaining_seconds = seconds; 
+			// 1 minute = 60 seconds
+			int seconds_per_minute = 60; 
+			// 1 hour = 60 minute (each minute is 60 seconds)
+			int seconds_per_hour = 60 * 60; 
+			// 1 day = 24 hours (each hour is 60 minutes; and each minute is 60 seconds)
+			int seconds_per_day = 24 * 60 * 60; 
+
+ 			// Divide the "remaining_seconds" with "number of seconds per day"
+			int days = remaining_seconds / seconds_per_day;
+			// Subtract the number of seconds which equivalent to the number of days calculated above
+			remaining_seconds -= days * seconds_per_day; 
+			// Divide the "remaining_seconds" with "number of seconds per hour"
+			int hours = remaining_seconds / seconds_per_hour; 
+			// Subtract the number of seconds which equivalent to the number of hours calculated above
+			remaining_seconds -= hours * seconds_per_hour; 
+			// Divide the "remaining_seconds" with "number of seconds per minute"
+			int minutes = remaining_seconds / seconds_per_minute;
+			// Subtract the number of seconds which equivalent to the minutes of days calculated above
+			remaining_seconds -= minutes * seconds_per_minute; 
+
+			System.Console.WriteLine(days + " days, " + hours + " hours, " + minutes + " minutes, " + remaining_seconds + " seconds");
+			System.Console.ReadKey();
 		}
 	}
 }
